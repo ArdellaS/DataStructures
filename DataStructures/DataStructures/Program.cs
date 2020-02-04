47f92a6dbd2012faf08812d97c822fb36dc2544b
Shelton <dellashelton@quickenloans.com>
2020-02-04 09:10:06 -0500

    final spice

--- DataStructures/DataStructures/Program.cs
+++ DataStructures/DataStructures/Program.cs
@@ -12,30 +12,23 @@ namespace DataStructures
             Console.Write("Please eter a word you would like to reverse: ");
             string word = Console.ReadLine();
 
-            Reverse(word);
-            //Console.WriteLine(Reverse(""));
-
-            //string word = GetString("Please enter a word you would like to revsere: ");
-
-        }
-
-        static string Reverse(string str)
-        {
-            string word = str;
-
             Stack<char> letters = new Stack<char>();
 
-            while (word is string)
-            {
+            
                 foreach (var letter in word)
                 {
                     letters.Push(letter);
                 }
 
+            Console.Write($"{ word} :");
+            for (int i = 0; i < word.Length; i++)
+            {
+            Console.Write($" {letters.Pop()}");
             }
 
-            return letters.ToString();
         }
+
+        
     }
 
 }
